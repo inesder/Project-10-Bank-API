61907347c1254e8b6a393e7c09e0131297f55401
inesder <inesderraz@hotmail.fr>
2024-11-13 19:14:13 +0100

    User can only see their own profile, their name and the design #4

--- argentbank/src/pages/ProfilePage.jsx
+++ argentbank/src/pages/ProfilePage.jsx
@@ -4,7 +4,7 @@ import { userProfile } from '../features/user/userActions';
 
 
 function ProfilePage(){
-    const { userInfos, loading, error } = useSelector((state) => state.user)
+    const { userInfos} = useSelector((state) => state.user)
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -12,7 +12,43 @@ function ProfilePage(){
     }, [dispatch]);
 
     return(
-    <h1>my profile {userInfos ? userInfos.firstName : "Loading..."}</h1>
+    <main className="main bg-dark">
+    <div className="header">
+    <h1>Welcome back <br/>{userInfos.firstName} {userInfos.lastName}</h1>
+    <button className="edit-button">Edit Name</button>
+      </div>
+      <h2 className="sr-only">Accounts</h2>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+          <p className="account-amount">$2,082.79</p>
+          <p className="account-amount-description">Available Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+          <p className="account-amount">$10,928.42</p>
+          <p className="account-amount-description">Available Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+          <p className="account-amount">$184.30</p>
+          <p className="account-amount-description">Current Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+    </main>
 )
 }
 
